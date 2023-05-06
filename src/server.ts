8275824a6946b66d4eda2a0f6a579b6a4e33f6da
Anthony Victor <thonnyvrc@gmail.com>
2023-05-05 22:29:45 -0300

    config: added cors lib

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express from "express";
 import * as dotenv from "dotenv";
 import router from "./routes";
 import { connectToMongoDb } from "./infra/mongodb/config";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
